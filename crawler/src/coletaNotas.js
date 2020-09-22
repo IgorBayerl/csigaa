eca2fcea7344d8ba8767a7ce1340b51f1b41328b
Igor Bayerl <bayerl.igor@gmail.com>
2020-09-22 20:46:07 -0300

    coleta notas
    Coleta notas e as adiciona em um array de objetos

---
+++ crawler/src/coletaNotas.js
@@ -0,0 +1,42 @@
+
+
+async function coletaNotas (page){
+
+    await page.waitForNavigation();
+    const notas = await page.evaluate(() => {
+        const linhaCima = document.getElementById('trAval').querySelectorAll('th')
+        const comprimentoArray = linhaCima.length
+        const media = document.querySelectorAll('.linhaPar td')[comprimentoArray-5].textContent.trim()
+        const situacao = document.querySelectorAll('.linhaPar td')[comprimentoArray-1].textContent.trim()
+
+        let notas = []
+        for (let i = 0; i < comprimentoArray - 7; i++) {
+            const nota = document.querySelectorAll('.linhaPar td')[2 + i].textContent.trim()
+            const name = document.getElementById('trAval').querySelectorAll('th')[ 2 + i ].textContent.trim()
+            console.log(` Nota ${i} = ${nota}`)
+            const object = { name : name , nota : nota }
+            if(nota != null){
+                notas.push(object)
+            }
+        }
+
+        return{
+            linhaCima: linhaCima ,
+            quantidadeDeNotas: comprimentoArray - 7,
+            media: media,
+            situacao: situacao,
+            notas: notas
+        }
+    })
+    
+    
+    console.log(`[ Boletim ] - [ Media ] : ${notas.media}`)
+    console.log(`[ Boletim ] - [ Situacao ] : ${notas.situacao}`)
+    console.log(`[ Boletim ] - [ Notas ] : ${JSON.stringify(notas.notas)}`)
+
+    await page.goBack()
+    return notas
+}
+
+
+module.exports = coletaNotas
--- crawler/src/routes.js
+++ crawler/src/routes.js
@@ -1,12 +1,12 @@
 const puppeteer = require('puppeteer');
 const readlineSync = require('readline-sync');
-
+const coletaNotas = require('./coletaNotas')
 
 
 // OK  Fazer login no sigaa
 // OK coletar informações basicas do aluno ( nome, curso ...)
 // Limpar informações coletadas
-// verificar as materias que o aluno está matriculado 
+// OK verificar as materias que o aluno está matriculado 
 // Extrair essas materias para um array de objetos com cada objeto sendo uma disciplina
 // celetar as notas de cada disciplina
 // coletar as informações de presença de cada disciplina
@@ -14,52 +14,50 @@ const readlineSync = require('readline-sync');
 
 async function crawlerTest() {
 
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto('https://sig.ifc.edu.br/sigaa/verTelaLogin.do');
-
+    
     const userLogin = readlineSync.question('Informe seu usuario : ') ;
     const userSenha = readlineSync.question('Informe a sua senha : ') ;
 
-    const disciplina = 1
-
     //// Fazendo Login
+    
     await login(page, userLogin, userSenha);
 
     await page.waitForNavigation();
 
-
-    const name = await page.evaluate(() => {
-        return document.querySelector('.usuario span').title
-    });
-
-    const unidade = await page.evaluate(() => {
-        return document.querySelector('.unidade').textContent
-    });
-
-    const matricula = await page.evaluate(() => {
-        return document.querySelector('#agenda-docente table tbody tr').innerText
+    const dados = await page.evaluate(() => {
+        return {
+            name : document.querySelector('.usuario span').title.trim(),
+            matricula : document.querySelector('#agenda-docente table tbody tr').innerText.trim(),
+            unidade : document.querySelector('.unidade').textContent.trim(),
+            quantidadeDeMaterias : document.getElementsByClassName('descricao').length,
+        }
     });
 
     //// CONTANDO AS MATERIAS MATRICULADAS
-    const disciplinas = await page.evaluate(() => {
-        return document.getElementsByClassName('descricao').length
-    });
+    // const disciplinas = await page.evaluate(() => {
+    //     return document.getElementsByClassName('descricao').length
+    // });
 
     
-    console.log(`Bem vindo ${name}`)
-    console.log(matricula)
+    console.log(`Bem vindo ${dados.name}`)
+    console.log(dados.matricula)
 
+    await paraCadaDisciplina(page , dados.quantidadeDeMaterias)
+    
+    console.log('[ main ]: encerrando...')
+    await browser.close();
+}
+
+async function paraCadaDisciplina(page , disciplinas){
     for (let i = 0; i < disciplinas; i++) {
-        console.log(` disciplina ${i}`)
+        console.log(`- Disciplina ${i}`)
         await entrandoNasPaginasColetandoInformacoes(page, i)
         await page.waitForNavigation();
     }
-
-    // await browser.close();
 }
-
-
 
 async function login(page, userLogin, userSenha) {
 
@@ -85,17 +83,35 @@ async function clicandoNaDisciplina(page, disciplina) {
 }
 
 
+
 async function entrandoNasPaginasColetandoInformacoes(page , disciplina){
     
     await clicandoNaDisciplina(page, disciplina)
 
     // acessando pagina de notas
+    console.log(`[acessando disciplina ${disciplina}]: acessando pagina de notas...`)
     await page.evaluate(() => {
         document.getElementsByClassName('rich-panelbar rich-panelbar-interior ')[1].getElementsByClassName('itemMenu')[2].click()
     });
 
+    
+    const notas = await coletaNotas(page)
+
+    /// acessando pagina presença
+    console.log(`[acessando disciplina ${disciplina}]: acessando pagina de presença...`)
+    await page.evaluate(() => {
+        document.getElementsByClassName('rich-panelbar rich-panelbar-interior ')[1].getElementsByClassName('itemMenu')[0].click()
+    });
     await page.waitForNavigation();
-    await page.goBack()
+
+    const presencas = await page.evaluate(() => {
+        const linhasPar = document.getElementsByClassName('linhaPar').length
+        const linhasImpar = document.getElementsByClassName('linhaImpar').length
+        return{
+            quantidadeDeAulasRegistradas: linhasImpar + linhasPar,
+        }
+    })
+    console.log(`[ Presenca ] - [Aulas registradas]: ${presencas.quantidadeDeAulasRegistradas}`)
 
     /// voltando pra home
     await page.evaluate(() => {
